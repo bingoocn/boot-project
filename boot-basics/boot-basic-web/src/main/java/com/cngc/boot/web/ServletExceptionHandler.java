9921a0128e67621f12f19e8fea31ef787f5c2dc9
duanyinglong <kylerduane@163.com>
2019-08-07 18:06:51 +0800

    处理BindException异常

--- boot-basics/boot-basic-web/src/main/java/com/cngc/boot/web/ServletExceptionHandler.java
+++ boot-basics/boot-basic-web/src/main/java/com/cngc/boot/web/ServletExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -41,6 +42,12 @@ public class ServletExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handle(new RequestBodyValidationException(ex.getBindingResult().getFieldErrors()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(
+            BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return handle(new RequestBodyValidationException(ex.getBindingResult().getFieldErrors()));
     }
 
