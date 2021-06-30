80e801fe5c6eb6acd2926b9ba54ddc0dab1e8020
max <maxduane@163.com>
2021-06-30 13:41:06 +0800

    增加sdkclient的实现

--- boot-integrations/boot-integration-gmp/src/main/java/com/cngc/boot/integration/gmp/config/IntegrationConfiguration.java
+++ boot-integrations/boot-integration-gmp/src/main/java/com/cngc/boot/integration/gmp/config/IntegrationConfiguration.java
@@ -1,11 +1,14 @@
 package com.cngc.boot.integration.gmp.config;
 
 import com.cngc.boot.integration.gmp.DictTranslateServiceImpl;
+import com.cngc.boot.security.authentication.LoginUser;
 import com.cngc.boot.web.dictionary.service.DictTranslateService;
+import org.jeecg.common.system.sdk.GmpSdkClient;
 import org.jeecg.common.system.sdk.service.IAdminDictService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * 集成配置类.
@@ -19,5 +22,16 @@ public class IntegrationConfiguration {
     @ConditionalOnMissingBean
     public DictTranslateService dictTranslateService(IAdminDictService adminDictService) {
         return new DictTranslateServiceImpl(adminDictService);
+    }
+
+    @Bean
+    public GmpSdkClient gmpSdkClient() {
+        return new GmpSdkClient() {
+            @Override
+            public String getUserAccount() {
+                LoginUser user = (LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                return user.getAccount();
+            }
+        };
     }
 }
